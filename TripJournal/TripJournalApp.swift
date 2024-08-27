66b9958b6f229b394e56ec8e8ee0e89c78c9b2a1
Erica Sampson <ericabcherry@gmail.com>
2024-08-27 11:00:12 -0400

    Initial Commit

---
+++ TripJournal/TripJournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripJournalApp.swift
+//  TripJournal
+//
+//  Created by Erica Sampson on 2024-08-27.
+//
+
+import SwiftUI
+
+@main
+struct TripJournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
